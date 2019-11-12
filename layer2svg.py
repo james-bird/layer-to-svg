66f1f521b10680834c770addfc9b6b211568004f
frankiezafe <frankie@frankiezafe.org>
2019-11-12 18:26:29 +0100

    generation of a folder having the same name as the svg and generation of svg in this folder

--- layer2svg.py
+++ layer2svg.py
@@ -1,29 +1,33 @@
 #! /usr/bin/python3
 import xml.etree.ElementTree as ET
+import os
 import copy
 import sys
 
+export_folder = os.path.splitext(sys.argv[1])[0]
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 listoflayers=[]
 for g in root.findall('{http://www.w3.org/2000/svg}g'):
-    name = g.get('{http://www.inkscape.org/namespaces/inkscape}label')
-    listoflayers.append(name)
+	name = g.get('{http://www.inkscape.org/namespaces/inkscape}label')
+	listoflayers.append(name)
 print(listoflayers)
+
+if not os.path.exists(export_folder):
+	os.makedirs(export_folder)
+
 try:
-    listoflayers.remove('background')
+	listoflayers.remove('background')
 except ValueError:
-    print("No background")
+	print("No background")
 for counter in range(len(listoflayers)):
-    james=listoflayers[:]
-    temp_tree = copy.deepcopy(tree)
-    del james[counter]
-    print(james)
-    temp_root = temp_tree.getroot()
-    for g in temp_root.findall('{http://www.w3.org/2000/svg}g'):
-        name = g.get('{http://www.inkscape.org/namespaces/inkscape}label')
-        if name in james:
-            temp_root.remove(g)
-    temp_tree.write(listoflayers[counter]+'.svg')
-
-
+	james=listoflayers[:]
+	temp_tree = copy.deepcopy(tree)
+	del james[counter]
+	print(james)
+	temp_root = temp_tree.getroot()
+	for g in temp_root.findall('{http://www.w3.org/2000/svg}g'):
+		name = g.get('{http://www.inkscape.org/namespaces/inkscape}label')
+		if name in james:
+			temp_root.remove(g)
+	temp_tree.write(os.path.join(export_folder,listoflayers[counter]+'.svg'))
